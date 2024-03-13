ec7ebe2ae3b9a5e0fc8db77cca73219744173740
RAJ RANJAN SHRIVASTAVA <141056721+Raj3051@users.noreply.github.com>
2024-03-13 20:18:24 +0530

    Add files via upload

---
+++ 12LEDs-1.ino
@@ -0,0 +1,440 @@
+int red2=3;
+int red3=4;
+int red4=5;
+int red5=6;
+int red6=7;
+int red7=8;
+int red8=9;
+int red9=10;;
+int red10=11;
+int red11=12;
+int red12=13;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode (red2,OUTPUT);
+pinMode (red3,OUTPUT);
+pinMode (red4,OUTPUT);
+pinMode (red5,OUTPUT);
+pinMode (red6,OUTPUT);
+pinMode (red7,OUTPUT);
+pinMode (red8,OUTPUT);
+pinMode (red9,OUTPUT);
+pinMode (red10,OUTPUT);
+pinMode (red11,OUTPUT);
+pinMode (red12,OUTPUT);
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite (red2,1);
+digitalWrite (red3,0);
+digitalWrite (red4,0);
+digitalWrite (red5,0);
+digitalWrite (red6,0);
+digitalWrite (red7,0);
+digitalWrite (red8,0);
+digitalWrite (red9,0);
+digitalWrite (red10,0);
+digitalWrite (red11,0);
+digitalWrite (red12,0);
+delay(500);
+digitalWrite (red2,1);
+digitalWrite (red3,1);
+digitalWrite (red4,0);
+digitalWrite (red5,0);
+digitalWrite (red6,0);
+digitalWrite (red7,0);
+digitalWrite (red8,0);
+digitalWrite (red9,0);
+digitalWrite (red10,0);
+digitalWrite (red11,0);
+digitalWrite (red12,0);
+delay(500);
+digitalWrite (red2,1);
+digitalWrite (red3,1);
+digitalWrite (red4,1);
+digitalWrite (red5,0);
+digitalWrite (red6,0);
+digitalWrite (red7,0);
+digitalWrite (red8,0);
+digitalWrite (red9,0);
+digitalWrite (red10,0);
+digitalWrite (red11,0);
+digitalWrite (red12,0);
+delay(500);
+digitalWrite (red2,1);
+digitalWrite (red3,1);
+digitalWrite (red4,1);
+digitalWrite (red5,1) ;
+digitalWrite (red6,0);
+digitalWrite (red7,0);
+digitalWrite (red8,0);
+digitalWrite (red9,0);
+digitalWrite (red10,0);
+digitalWrite (red11,0);
+digitalWrite (red12,0);
+delay(500);
+digitalWrite (red2,1);
+digitalWrite (red3,1);
+digitalWrite (red4,1);
+digitalWrite (red5,1) ;
+digitalWrite (red6,1);
+digitalWrite (red7,0);
+digitalWrite (red8,0);
+digitalWrite (red9,0);
+digitalWrite (red10,0);
+digitalWrite (red11,0);
+digitalWrite (red12,0);
+delay(500);
+digitalWrite (red2,1);
+digitalWrite (red3,1);
+digitalWrite (red4,1);
+digitalWrite (red5,1) ;
+digitalWrite (red6,1);
+digitalWrite (red7,1);
+digitalWrite (red8,0);
+digitalWrite (red9,0);
+digitalWrite (red10,0);
+digitalWrite (red11,0);
+digitalWrite (red12,0);
+delay(500);
+digitalWrite (red2,1);
+digitalWrite (red3,1);
+digitalWrite (red4,1);
+digitalWrite (red5,1) ;
+digitalWrite (red6,1);
+digitalWrite (red7,1);
+digitalWrite (red8,1);
+digitalWrite (red9,0);
+digitalWrite (red10,0);
+digitalWrite (red11,0);
+digitalWrite (red12,0);
+delay(500);
+digitalWrite (red2,1);
+digitalWrite (red3,1);
+digitalWrite (red4,1);
+digitalWrite (red5,1) ;
+digitalWrite (red6,1);
+digitalWrite (red7,1);
+digitalWrite (red8,1);
+digitalWrite (red9,1);
+digitalWrite (red10,0);
+digitalWrite (red11,0);
+digitalWrite (red12,0);
+delay(500);
+digitalWrite (red2,1);
+digitalWrite (red3,1);
+digitalWrite (red4,1);
+digitalWrite (red5,1) ;
+digitalWrite (red6,1);
+digitalWrite (red7,1);
+digitalWrite (red8,1);
+digitalWrite (red9,1);
+digitalWrite (red10,1);
+digitalWrite (red11,0);
+digitalWrite (red12,0);
+delay(500);
+digitalWrite (red2,1);
+digitalWrite (red3,1);
+digitalWrite (red4,1);
+digitalWrite (red5,1) ;
+digitalWrite (red6,1);
+digitalWrite (red7,1);
+digitalWrite (red8,1);
+digitalWrite (red9,1);
+digitalWrite (red10,1);
+digitalWrite (red11,1);
+digitalWrite (red12,0);
+delay(500);
+digitalWrite (red2,1);
+digitalWrite (red3,1);
+digitalWrite (red4,1);
+digitalWrite (red5,1) ;
+digitalWrite (red6,1);
+digitalWrite (red7,1);
+digitalWrite (red8,1);
+digitalWrite (red9,1);
+digitalWrite (red10,1);
+digitalWrite (red11,1);
+digitalWrite (red12,1);
+delay(500);
+digitalWrite (red2,0);
+digitalWrite (red3,0);
+digitalWrite (red4,0);
+digitalWrite (red5,0);
+digitalWrite (red6,0);
+digitalWrite (red7,0);
+digitalWrite (red8,0);
+digitalWrite (red9,0);
+digitalWrite (red10,0);
+digitalWrite (red11,0);
+digitalWrite (red12,0);
+delay (100);
+digitalWrite (red2,1);
+digitalWrite (red3,0);
+digitalWrite (red4,0);
+digitalWrite (red5,0);
+digitalWrite (red6,0);
+digitalWrite (red7,0);
+digitalWrite (red8,0);
+digitalWrite (red9,0);
+digitalWrite (red10,0);
+digitalWrite (red11,0);
+digitalWrite (red12,0);
+delay (200);
+digitalWrite (red2,0);
+digitalWrite (red3,1);
+digitalWrite (red4,0);
+digitalWrite (red5,0);
+digitalWrite (red6,0);
+digitalWrite (red7,0);
+digitalWrite (red8,0);
+digitalWrite (red9,0);
+digitalWrite (red10,0);
+digitalWrite (red11,0);
+digitalWrite (red12,0);
+delay (200);
+digitalWrite (red2,0);
+digitalWrite (red3,0);
+digitalWrite (red4,1);
+digitalWrite (red5,0);
+digitalWrite (red6,0);
+digitalWrite (red7,0);
+digitalWrite (red8,0);
+digitalWrite (red9,0);
+digitalWrite (red10,0);
+digitalWrite (red11,0);
+digitalWrite (red12,0);
+delay (200);
+digitalWrite (red2,0);
+digitalWrite (red3,0);
+digitalWrite (red4,0);
+digitalWrite (red5,1);
+digitalWrite (red6,0);
+digitalWrite (red7,0);
+digitalWrite (red8,0);
+digitalWrite (red9,0);
+digitalWrite (red10,0);
+digitalWrite (red11,0);
+digitalWrite (red12,0);
+delay (200);
+digitalWrite (red2,0);
+digitalWrite (red3,0);
+digitalWrite (red4,0);
+digitalWrite (red5,0);
+digitalWrite (red6,1);
+digitalWrite (red7,0);
+digitalWrite (red8,0);
+digitalWrite (red9,0);
+digitalWrite (red10,0);
+digitalWrite (red11,0);
+digitalWrite (red12,0);
+delay (200);
+digitalWrite (red2,0);
+digitalWrite (red3,0);
+digitalWrite (red4,0);
+digitalWrite (red5,0);
+digitalWrite (red6,0);
+digitalWrite (red7,1);
+digitalWrite (red8,0);
+digitalWrite (red9,0);
+digitalWrite (red10,0);
+digitalWrite (red11,0);
+digitalWrite (red12,0);
+delay (200);
+digitalWrite (red2,0);
+digitalWrite (red3,0);
+digitalWrite (red4,0);
+digitalWrite (red5,0);
+digitalWrite (red6,0);
+digitalWrite (red7,0);
+digitalWrite (red8,1);
+digitalWrite (red9,0);
+digitalWrite (red10,0);
+digitalWrite (red11,0);
+digitalWrite (red12,0);
+delay (200);
+digitalWrite (red2,0);
+digitalWrite (red3,0);
+digitalWrite (red4,0);
+digitalWrite (red5,0);
+digitalWrite (red6,0);
+digitalWrite (red7,0);
+digitalWrite (red8,0);
+digitalWrite (red9,1);
+digitalWrite (red10,0);
+digitalWrite (red11,0);
+digitalWrite (red12,0);
+delay (200);
+digitalWrite (red2,0);
+digitalWrite (red3,0);
+digitalWrite (red4,0);
+digitalWrite (red5,0);
+digitalWrite (red6,0);
+digitalWrite (red7,0);
+digitalWrite (red8,0);
+digitalWrite (red9,0);
+digitalWrite (red10,1);
+digitalWrite (red11,0);
+digitalWrite (red12,0);
+delay (200);
+digitalWrite (red2,0);
+digitalWrite (red3,0);
+digitalWrite (red4,0);
+digitalWrite (red5,0);
+digitalWrite (red6,0);
+digitalWrite (red7,0);
+digitalWrite (red8,0);
+digitalWrite (red9,0);
+digitalWrite (red10,0);
+digitalWrite (red11,1);
+digitalWrite (red12,0);
+delay (200);
+digitalWrite (red2,0);
+digitalWrite (red3,0);
+digitalWrite (red4,0);
+digitalWrite (red5,0);
+digitalWrite (red6,0);
+digitalWrite (red7,0);
+digitalWrite (red8,0);
+digitalWrite (red9,0);
+digitalWrite (red10,0);
+digitalWrite (red11,0);
+digitalWrite (red12,1);
+delay (500);
+digitalWrite (red12,1);
+delay (200);
+digitalWrite (red11,1);
+delay (200);
+digitalWrite (red10,1);
+delay (200);
+digitalWrite (red9,1);
+delay (200);
+digitalWrite (red8,1);
+delay (200);
+digitalWrite (red7,1);
+delay (200);
+digitalWrite (red6,1);
+delay (200);
+digitalWrite (red5,1);
+delay (200);
+digitalWrite (red4,1);
+delay (200);
+digitalWrite (red3,1);
+delay (200);
+digitalWrite (red2,1);
+delay (200);
+for( int a = 0; a < 3; a = a +1 )  //Blink led 3 times
+  {  
+  digitalWrite(13, HIGH);  
+  delay(500);   
+  digitalWrite(13, LOW);  
+  delay(500); 
+  }
+for( int b = 0; b < 3; b = b +1 )  
+  {  
+  digitalWrite(12, HIGH);  
+  delay(500);   
+  digitalWrite(12, LOW);  
+  delay(500); 
+  }
+for( int c = 0; c < 3; c = c +1 )  
+  {  
+  digitalWrite(11, HIGH);  
+  delay(500);   
+  digitalWrite(11, LOW);  
+  delay(500); 
+  }
+for( int d = 0; d < 3; d = d +1 )  //Blink led 3 times
+  {  
+  digitalWrite(10, HIGH);  
+  delay(500);   
+  digitalWrite(10, LOW);  
+  delay(500); 
+  }
+for( int e = 0; e < 3; e = e +1 )  
+  {  
+  digitalWrite(9, HIGH);  
+  delay(500);   
+  digitalWrite(9, LOW);  
+  delay(500); 
+  }
+for( int f = 0; f < 3; f = f +1 )  
+  {  
+  digitalWrite(8, HIGH);  
+  delay(500);   
+  digitalWrite(8, LOW);  
+  delay(500); 
+  }
+for( int g = 0; g < 3; g = g +1 )  //Blink led 3 times
+  {  
+  digitalWrite(7, HIGH);  
+  delay(500);   
+  digitalWrite(7, LOW);  
+  delay(500); 
+  }
+for( int h = 0; h < 3; h = h +1 )  
+  {  
+  digitalWrite(6, HIGH);  
+  delay(500);   
+  digitalWrite(6, LOW);  
+  delay(500); 
+  }
+for( int i = 0; i < 3; i = i +1 )  
+  {  
+  digitalWrite(5, HIGH);  
+  delay(500);   
+  digitalWrite(5, LOW);  
+  delay(500); 
+  }
+for( int j = 0; j < 3; j = j +1 )  //Blink led 3 times
+  {  
+  digitalWrite(4, HIGH);  
+  delay(500);   
+  digitalWrite(4, LOW);  
+  delay(500); 
+  }
+for( int k = 0; k < 3; k = k +1 )  
+  {  
+  digitalWrite(3, HIGH);  
+  delay(500);   
+  digitalWrite(3, LOW);  
+  delay(500); 
+  }
+for( int l = 0; l < 3; l = l +1 )  
+  {  
+  digitalWrite(2, HIGH);  
+  delay(500);   
+  digitalWrite(2, LOW);  
+  delay(500); 
+  }
+for( int m = 0; m < 3; m = m +1 )  
+  {  
+  digitalWrite (red2,1);
+  digitalWrite (red3,1);
+  digitalWrite (red4,1);
+  digitalWrite (red5,1) ;
+  digitalWrite (red6,1);
+  digitalWrite (red7,1);
+  digitalWrite (red8,1);
+  digitalWrite (red9,1);
+  digitalWrite (red10,1);
+  digitalWrite (red11,1);
+  digitalWrite (red12,1);
+  delay(500);
+  digitalWrite (red2,0);
+  digitalWrite (red3,0);
+  digitalWrite (red4,0);
+  digitalWrite (red5,0);
+  digitalWrite (red6,0);
+  digitalWrite (red7,0);
+  digitalWrite (red8,0);
+  digitalWrite (red9,0);
+  digitalWrite (red10,0);
+  digitalWrite (red11,0);
+  digitalWrite (red12,0);
+  delay(500);
+  }
+}
